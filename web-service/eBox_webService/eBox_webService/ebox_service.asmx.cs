c2e12ed52c5204958530ad8efdbaeac55327953d
stone-kee <kb2008@live.cn>
2015-11-21 11:32:20 +0800

    webservice
    Signed-off-by: stone-kee <kb2008@live.cn>

---
+++ web-service/eBox_webService/eBox_webService/ebox_service.asmx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace eBox_webService
+{
+    /// <summary>
+    /// ebox_service 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。
+    // [System.Web.Script.Services.ScriptService]
+    public class ebox_service : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
