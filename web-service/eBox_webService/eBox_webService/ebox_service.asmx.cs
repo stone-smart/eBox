c01974e39ca976da6d5fdbd678939ea776293a9e
stone-kee <kb2008@live.cn>
2015-11-21 13:22:21 +0800

    debug_webservice
    Signed-off-by: stone-kee <kb2008@live.cn>

--- web-service/eBox_webService/eBox_webService/ebox_service.asmx.cs
+++ web-service/eBox_webService/eBox_webService/ebox_service.asmx.cs
@@ -9,7 +9,7 @@ namespace eBox_webService
     /// <summary>
     /// ebox_service 的摘要说明
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "http://eboxServic.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消对下行的注释。
@@ -24,9 +24,34 @@ namespace eBox_webService
         }
 
         [WebMethod]
+        //加法
         public int add(int a, int b)
         {
             return (a + b);
         }
+
+        [WebMethod]
+        //快递员登录信息验证
+        public Boolean Postman_Verify(string PhoneNo, string Psw)
+        {
+
+            return true;
+        }
+
+        [WebMethod]
+        //管理员登录验证
+        public Boolean Manager_Verify(string PhoneNo, string Psw)
+        {
+
+            return true;
+        }
+
+        [WebMethod]
+        //上传存取包记录
+        public Boolean Uploadlog(string log)
+        {
+            return true;
+        }
+        
     }
 }
---
+++ web-service/test_webService/Test_WinForm/Test_WinForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //初始化对象
+            eBox_WebServices.ebox_serviceSoapClient webClient= new eBox_WebServices.ebox_serviceSoapClient();
+            int a,b;
+            a=int.Parse(textBox1.Text);
+            b=int.Parse(textBox2.Text);
+            //调用web服务
+            label1.Text = webClient.add(a, b).ToString();           
+        }
+    }
+}
